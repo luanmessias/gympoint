959ec73713419ed34f0cd66ae082346fb467e43e
luanmessias <luanmessias10@gmail.com>
2019-11-14 20:19:04 -0300

    Add eslint, prettier and editor config patterns

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello Gympoint" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Gympoint' });
 });
 
 export default routes;
