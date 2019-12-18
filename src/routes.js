58f77f71f856aaae630442911b562367971b234f
luanmessias <luanmessias10@gmail.com>
2019-12-18 18:49:18 +0000

    Add User Model and Controller to project

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
-const routes = Router();
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Gympoint' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
